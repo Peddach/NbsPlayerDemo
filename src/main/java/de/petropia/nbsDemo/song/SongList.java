f4227f6aed038f925e882a8b5e697ed0a7e62426
ProfessorSam <Samderprofessor@gmail.com>
2022-05-23 21:20:14 +0200

    ↗ Loading logic for every song

--- src/main/java/de/petropia/nbsDemo/song/SongList.java
+++ src/main/java/de/petropia/nbsDemo/song/SongList.java
@@ -4,12 +4,16 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.xxmicloxx.NoteBlockAPI.model.Song;
+import com.xxmicloxx.NoteBlockAPI.utils.NBSDecoder;
+
 import de.petropia.nbsDemo.NbsDemo;
 
 public class SongList {
 	
 	private final File directory;
 	private final List<File> nbsFiles = new ArrayList<>();
+	private final List<Song> songs = new ArrayList<>();
 	
 	public SongList(String path) {
 		directory = new File(path);
@@ -19,6 +23,13 @@ public class SongList {
 		}
 		loadNbsFiles();
 		NbsDemo.getInstance().getLogger().info(nbsFiles.size() + " Dateien wurden geladen");
+		loadSongsFromNbs();
+	}
+	
+	private void loadSongsFromNbs() {
+		for(File file : nbsFiles) {
+			songs.add(NBSDecoder.parse(file));
+		}
 	}
 	
 	private void loadNbsFiles() {
@@ -42,5 +53,9 @@ public class SongList {
 	
 	private void createDirectory() {
 		directory.mkdirs();
+	}
+
+	public List<Song> getSongs() {
+		return songs;
 	}
 }
